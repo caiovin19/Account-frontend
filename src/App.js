0b962f5921fce2ba27201c3e7d1d68096c8079fc
caiovin19 <caio.vin19@outlook.com>
2022-12-24 20:30:07 -0300

    feat(Pegando dados do Banco); - Fazendo get do banco após inserir valores dentro do Input de números da conta;

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 
 import './App.css';
-import Transferencias from './Transferencias';
 import Conta from './Conta';
-import { useEffect, useState } from 'react';
 function App() {
-  const[conta, setContas]=useState([]);
-
-  useEffect(()=>{
-  fetch("http://localhost:8080/contas")
-  .then(retorno=>retorno.json())
-  .then(retorno_convertido=>setContas(retorno_convertido));
-  },[]);
   
   return (
     <div>
       <Conta/>
-      <Transferencias vetor={conta}/>
     </div>
   );
 }
--- src/Conta.js
+++ src/Conta.js
@@ -1,12 +1,91 @@
-function Conta(){
-    return(
-    <form>
-        <input type='date' placeholder='Data de início'/>
-        <input type='date' placeholder='Data de Fim'/>
-        <input type='number' placeholder='Nome operador transacionado'/>
+import { useState, useCallback } from "react";
+import api from './plugins/axios';
 
-        <input type='button' value='Pesquisar' className='btn btn-primary'/>
-    </form>
+function Conta(){
+
+    
+    const [conta, setContas]=useState({});
+    const [value, setValue] = useState({
+        value: '',
+    })
+
+    const [dateStart, setDateStart] = useState({
+        value: '',
+    })
+
+    const [dateEnd, setDateEnd] = useState({
+        value: '',
+    })
+
+    const handleChanging = (e) => {
+        setValue({value: e.target.value})
+    }
+
+    const handleStartChanging = (e) => {
+        let dataInArray = e.target.value.split('-')
+        let dataInvertida = dataInArray[2]+'-'+dataInArray[1]+'-'+dataInArray[0];
+        setDateStart({value: dataInvertida})
+    }
+
+    const handleEndChanging = (e) => {
+        let dataInArray = e.target.value.split('-')
+        let dataInvertida = dataInArray[2]+'-'+dataInArray[1]+'-'+dataInArray[0];
+        setDateEnd({value: dataInvertida})
+    }
+
+    const getContas = useCallback( async () => {
+        let id = value.value;
+        if(value.value.length == 0 || value.value === '') {
+            alert('Por favor, campos precisam ser preenchidos!');
+            return;
+        }
+        await api.get(`/contas/${id}`)
+            .then( res => {
+                setContas(res.data);
+                console.log(res.data)
+            })
+            .catch( error => {
+                console.warn(error);
+            })
+    });
+    
+    return(
+        <>
+            <form>
+                <input type='date' onChange={handleStartChanging} value={dateStart.value} placeholder='Data de início'/>
+                <input type='date' onChange={handleEndChanging} value={dateEnd.value} placeholder='Data de Fim'/>
+                <input id="idAccount" type='number' value={value.value} onChange={handleChanging} placeholder='Nome operador transacionado'/>
+
+                <input type='button' onClick={getContas} value='Pesquisar' className='btn btn-primary'/>
+            </form>
+            
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th scope="col">Dados</th>
+                        <th scope="col">Valentia</th>
+                        <th scope="col">Tipo</th>
+                        <th scope="col">Nome operador transacionado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        {
+                            conta?.transferencias?.map((data) => {
+                                return (
+                                    <>
+                                        <tr key={conta.transferencias.length}>
+                                            <th scope="row">{conta.idConta}</th>
+                                            <td>R$ {data.valor}</td>
+                                            <td>{data.tipo}</td>
+                                            <td>{conta.nomeResponsavel}</td>
+                                        </tr>
+                                    </>
+                                )
+                            })
+                        }
+                </tbody>
+            </table>
+        </>
     )
 }
 
